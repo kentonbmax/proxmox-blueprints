ac010e60ce2d54799a6aa98542b41736a3ede4eb
Kenton Bocock <kentonbo@gmail.com>
2023-02-19 14:26:57 -0500

    add ansible setup

---
+++ scripts/setup-image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt install libguestfs-tools
+read -r -p 'Give me a imagename: ' value
+printf 'You gave me [%s].\n' "$value"
+virt-customize -a $value --update
+virt-customize -a $value --install qemu-guest-agent
+virt-customize -a $value --run-command 'useradd --shell /bin/bash ansible'
+virt-customize -a $value --run-command 'mkdir -p /home/ansible/.ssh'
+virt-customize -a $value --ssh-inject root:file:/root/.ssh/ansible.pub
+virt-customize -a $value --run-command 'chown -R ansible:ansible /home/ansible'
+virt-customize -a $value --append-line '/etc/sudoers.d/ansible:ansible ALL=(ALL) NOPASSWD: ALL'
+virt-customize -a $value --run-command 'chmod 0440 /etc/sudoers.d/ansible'
+virt-customize -a $value --run-command 'chown root:root /etc/sudoers.d/ansible'
+virt-customize -a $value --run-command '>/etc/machine-id' # important step so your clones get unique mac address / network details.
