3bcddd438f281754a6b401b2c92df4efbefda987
Kenton Bocock <kentonbo@gmail.com>
2023-02-20 12:26:49 -0500

    fix user

--- scripts/setup-ansible-image.sh
+++ scripts/setup-ansible-image.sh
@@ -13,7 +13,7 @@ virt-customize -a $value --update
 virt-customize -a $value --install qemu-guest-agent
 virt-customize -a $value --run-command 'useradd --shell /bin/bash ansible'
 virt-customize -a $value --run-command 'mkdir -p /home/ansible/.ssh'
-virt-customize -a $value --ssh-inject root:file:/root/.ssh/ansible.pub
+virt-customize -a $value --ssh-inject ansible:file:/root/.ssh/ansible.pub
 virt-customize -a $value --run-command 'chown -R ansible:ansible /home/ansible'
 virt-customize -a $value --append-line '/etc/sudoers.d/ansible:ansible ALL=(ALL) NOPASSWD: ALL'
 virt-customize -a $value --run-command 'chmod 0440 /etc/sudoers.d/ansible'
