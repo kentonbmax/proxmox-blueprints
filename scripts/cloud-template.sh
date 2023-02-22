0de12ec01c4ed4ce6bbdef726ab116f0bda25d76
Kenton Bocock <kentonbo@gmail.com>
2023-02-21 19:54:57 -0500

    add ability to set storage

--- scripts/cloud-template.sh
+++ scripts/cloud-template.sh
@@ -7,21 +7,24 @@ echo "imagename:: $imagename"
 qemu-img resize focal-server-cloudimg-amd64.img 20g
 # configure ansible?
 read -r -p 'Setup for Asible? (y|n): ' ansb
+
 if $ansb -eq 'y'
 then
     sh ./script.sh
 fi
 
+# we might be on different storage types
+read -r -p 'Storage Type? (zfs|lvm): ' storage_type
+echo 'You set storage type: $storage_type'
+
+full_storage='local-$storage_type'
+
 # Create the VM
 qm create 9001 --memory 2048 --name ubuntu2204-ansible --net0 virtio,bridge=vmbr0
-qm importdisk 9001 focal-server-cloudimg-amd64.img local-lvm
+qm importdisk 9001 focal-server-cloudimg-amd64.img $full_storage
 # Configure the VM
-qm set 9001 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-9001-disk-0
-qm set 9001 --ide2 local-lvm:cloudinit
+qm set 9001 --scsihw virtio-scsi-pci --scsi0 $full_storage:vm-9001-disk-0
+qm set 9001 --ide2 $full_storage:cloudinit
 qm set 9001 --boot c --bootdisk scsi0
 qm set 9001 --serial0 socket --vga serial0
 qm set 9001 --agent enabled=1
-qm unlock 9001
-
-# Make the Template
-qm template 9001
