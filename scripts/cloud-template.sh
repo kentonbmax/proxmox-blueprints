a20a1e78386da2e70738638fba89765e51867ec2
Kenton Bocock <kentonbo@gmail.com>
2023-03-03 14:15:20 -0500

    cleanup

--- scripts/cloud-template.sh
+++ scripts/cloud-template.sh
@@ -20,7 +20,4 @@ qm set 9001 --ide2 local-lvm:cloudinit
 qm set 9001 --boot c --bootdisk scsi0
 qm set 9001 --serial0 socket --vga serial0
 qm set 9001 --agent enabled=1
-qm unlock 9001
 
-# Make the Template
-qm template 9001
