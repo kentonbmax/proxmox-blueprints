16a88343401013d5d1599622e65d2a0bb5bd23cc
Kenton Bocock <kentonbo@gmail.com>
2023-03-03 12:59:33 -0500

    add comment

--- scripts/proxmox-setup.sh
+++ scripts/proxmox-setup.sh
@@ -2,3 +2,5 @@
 pveum role add TerraformProv -privs "Datastore.AllocateSpace Datastore.Audit Pool.Allocate Sys.Audit VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.CPU VM.Config.Cloudinit VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Monitor VM.PowerMgmt"
 pveum user add terraform@pve
 pveum aclmod / -user terraform@pve -role TerraformProv
+
+# Set Power State Here 
