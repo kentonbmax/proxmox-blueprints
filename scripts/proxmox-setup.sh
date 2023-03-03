4807be144d75347b2f2ef6d9c7b422982b539cf0
Kenton Bocock <kentonbo@gmail.com>
2023-03-03 12:58:59 -0500

    add role setup

--- scripts/proxmox-setup.sh
+++ scripts/proxmox-setup.sh
@@ -1,9 +1,4 @@
-# Todo:
-
-# automate proxmox setup.
-
-# - Firewall
-# - cpu for reduced power
-# - Cluster?
-# set powersave mode
-echo $()
+# Terraform/Ansible user
+pveum role add TerraformProv -privs "Datastore.AllocateSpace Datastore.Audit Pool.Allocate Sys.Audit VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.CPU VM.Config.Cloudinit VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Monitor VM.PowerMgmt"
+pveum user add terraform@pve
+pveum aclmod / -user terraform@pve -role TerraformProv
