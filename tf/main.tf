93d14a31625049d9c88f52707f04b53a08f5c7ee
Kenton Bocock <kentonbo@gmail.com>
2023-04-07 20:48:33 -0400

    update naming

--- tf/main.tf
+++ tf/main.tf
@@ -12,7 +12,7 @@ locals {
       },
       { 
         type = "work" 
-        name = "wrker2"
+        name = "wrkr2"
       }
     ]
 }
@@ -31,12 +31,6 @@ variable "proxmox_storage" {
   default = "vms"
 }
 
-resource "random_string" "hash" {
-  length  = 3
-  special = false
-  upper   = false
-}
-
 # resource is formatted to be "[type]" "[entity_name]" so in this case
 # we are looking to create a proxmox_vm_qemu entity named test_server
 resource "proxmox_vm_qemu" "ubuntu2204-ansible" {
@@ -44,7 +38,7 @@ resource "proxmox_vm_qemu" "ubuntu2204-ansible" {
     for node in local.nodes : node.name => node
   }
 
-  name = "${each.key}-${each.value.type}-${random_string.hash.result}" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
+  name = "${terraform.workspace}-${each.value.name}" #count.index starts at 0, so + 1 means this VM will be named test-vm-1 in proxmox
   # this now reaches out to the vars file. I could've also used this var above in the pm_api_url setting but wanted to spell it out up there. target_node is different than api_url. target_node is which node hosts the template and thus also which node will host the new VM. it can be different than the host you use to communicate with the API. the variable contains the contents "prox-1u"
   target_node = var.proxmox_host
   clone = var.proxmox_template_name
