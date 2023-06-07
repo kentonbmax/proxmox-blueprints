2bee734ff2b09e931fdb160599e5c98ea5340466
Josh R <67718610+jrock-83@users.noreply.github.com>
2023-06-06 20:43:14 -0400

    Update proxmox-setup.sh
    Added lines 6 and 7 to create token, and add roles for Terraform Provider

--- scripts/proxmox-setup.sh
+++ scripts/proxmox-setup.sh
@@ -3,5 +3,9 @@ pveum role add TerraformProv -privs "Datastore.AllocateSpace Datastore.Audit Poo
 pveum user add terraform@pve
 pveum aclmod / -user terraform@pve -role TerraformProv
 
+pveum user token add terraform@pve terraform@pve!mytoken
+pveum role modify TerraformProv -privs "Datastore.AllocateSpace Datastore.Audit Pool.Allocate Sys.Audit Sys.Console Sys.Modify VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.Cloudinit VM.Config.CPU VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.Monitor VM.PowerMgmt"
+
+
 # Set Power State Here 
 # todo..
