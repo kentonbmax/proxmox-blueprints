e3a1e7e3f856013a43d7637f282db1fc6c89a74c
Kenton Bocock <kentonbo@gmail.com>
2023-08-22 22:00:32 -0400

    Update proxmox-setup.sh

--- scripts/proxmox-setup.sh
+++ scripts/proxmox-setup.sh
@@ -1,7 +1,9 @@
 # Terraform/Ansible user
+# Sets up the role and the new user
 pveum role add TerraformProv -privs "Datastore.AllocateSpace Datastore.Audit Pool.Allocate Sys.Audit VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.CPU VM.Config.Cloudinit VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Monitor VM.PowerMgmt"
 pveum user add terraform@pve
 pveum aclmod / -user terraform@pve -role TerraformProv
 
 # Set Power State Here 
-# todo..
+apt install cpupower
+@reboot echo "powersave" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor >> /etc/crontab
