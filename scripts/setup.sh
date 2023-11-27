30a4f64d0234050e93f7ae195b32bafe82c0badc
Kenton Bocock <kentonbo@gmail.com>
2023-11-27 18:53:34 -0500

    move ansible option to setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,13 +8,22 @@
     echo "The current date is: $current_date"
 
     # pull latest scripts?
-    read -r -p 'Get latest scripts? (y|n): ' scrips
-    if [[ $scrips == 'y' ]]
+    read -r -p 'Run with Ansible? (y|n): ' scrips
+    if [[ $scrips != 'y' ]]
     then
-        wget -nc "$base_url/proxmox-setup.sh"
-        wget -nc "$base_url/cloud-template.sh"
-        wget -nc "$base_url/setup-ansible-image.sh"
+        # grab public key
+        read -r -p 'Enter Ansible pub key?: ' key
+        if [ $key -n ]
+        then
+            echo "$key" > /root/.ssh/ansible.pub
+            echo "Ansible pub setup."
+            exit 1
+        fi
     fi
+
+    wget -nc "$base_url/proxmox-setup.sh"
+    wget -nc "$base_url/cloud-template.sh"
+    wget -nc "$base_url/setup-ansible-image.sh"
 
     chmod +x *.sh
 
