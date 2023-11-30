065200049e2588eac34d36f9d288b311374941c0
Kenton Bocock <kentonbo@gmail.com>
2023-11-30 12:28:24 -0500

    add more perf tuning for proxmox intel. fix storag

--- scripts/proxmox-setup.sh
+++ scripts/proxmox-setup.sh
@@ -14,7 +14,32 @@ pveum aclmod /vms -user terraform@pve -role PVEAdmin
 pveum user token add terraform@pve tftoken --privsep=0
 # todo role need storage access?
 
+# setup powerstate
+# check for existing powerstate
+read -r -p 'Set powerstate default is ondemand(ondemand|performance|powersave): ' pstate
+if [[ -z "{$pstate}" ||  =~ ^\(powersave|performance|ondemand\) ]]
+then
+    echo "powerstate must be a valid value (ondemand|performance|powersave)"
+    exit 0
+else
+    echo "@reboot  root  echo "powersave" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor" >> /etc/crontab
+fi
 
+# set hault timer
+echo 50000 > /sys/module/kvm/parameter/halt_poll_ns
 
-# Set Power State
-echo "@reboot  root  echo "powersave" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor" >> /etc/crontab
+# enable io thread. 
+-drive if=none,id=drive0,file=$blk_dev,format=raw,cache=none,aio=native \
+
+-device virtio-blk-pci,num-queues=4,drive=drive0 \
+
+# intel xeon 3rd gen or newer
+echo 8 > /sys/devices/system/node/node0/hugepages/hugepages-1048576kB/nr_hugepages
+
+mkdir /dev/hugepage1G
+
+mount -t hugetlbfs -o pagesize=1G none /dev/hugepage1G
+
+numactl -N 0 -m 0 qemu-system-x86_64 --enable-kvm -M q35 -smp 4 -m 4G \
+
+-mem-path /dev/hugepage1G -hda vm.img -vnc :2
