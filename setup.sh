79098feddaf5cb91cf9afb0b45cb98c65d4c35f7
kentonbmax <kentonbo@gmail.com>
2024-11-03 09:45:05 -0500

    update new setup.sh for ansible

--- setup.sh
+++ setup.sh
@@ -10,3 +10,24 @@ echo "***_____------\ Welcome to Proxmox Ansible Setup /------______**"
 fi
 
 
+read -r -p 'Setup Terrafrom role and a token? (y|n): ' tfsetup
+if [[ $tfsetup == 'y' ]]
+then
+    # Setup Terraform role
+    echo "**** Setting up terraform role. ****"
+    # Terraform/Ansible user
+    pveum role add TerraformProv -privs "Datastore.AllocateSpace Datastore.Audit Pool.Allocate Sys.Audit Sys.Console Sys.Modify VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.Cloudinit VM.Config.CPU VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.Monitor VM.PowerMgmt"
+    pveum user add terraform@pve
+    pveum aclmod / -user terraform@pve -role TerraformProv
+    pveum aclmod /vms -user terraform@pve -role PVEAdmin
+    pveum acl modify / -user terraform@pve -role PVESDNUser
+fi
+
+if if [[ $tfsetup == 'y' ]]
+then
+    echo "**** Grab your token and user"
+    pveum user token add terraform@pve tftoken --privsep=0
+fi
+
+
+
